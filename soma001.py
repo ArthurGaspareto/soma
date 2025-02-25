68e4f162389e15eb0ec09e26ba74f95258195636
ArthurGaspareto <148639898+ArthurGaspareto@users.noreply.github.com>
2025-02-25 20:31:52 -0300

    Add files via upload

---
+++ soma001.py
@@ -0,0 +1,28 @@
+'''
+Programa: Soma
+Descrição: Armazena os números 2 e 3 e imprime a soma na tela 
+Autor: Arthur Gaspareto
+Data: 25/02/25
+Versão: 0.0.2
+Novidades da Versão
+
+25/02/2025
+Nesta versão:
+1.Armazena os números 2 e 3 e imprime a soma na tela
+2.O usuário digita os números a serem somados
+'''
+
+#Alocação de Memória
+soma = float()
+num1 = 2
+num2 = 3
+
+#Entrada de Dados
+
+#Processamento de Dados
+soma = num1 + num2
+
+
+#Saída de dados
+
+print(f'A soma dos números é: {soma}')
